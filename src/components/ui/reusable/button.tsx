cb3e84e9790dabffe7792fc538dcde74f28f1657
A1tBot <timofeyershovv@gmail.com>
2026-01-16 23:01:13 +0300

    Сделал кастомную кнопку (button)

--- src/components/ui/reusable/button.tsx
+++ src/components/ui/reusable/button.tsx
@@ -1,9 +1,39 @@
-import { FC } from "react";
+import "./css/button-style.css"
 
-export const Button: FC = ({}) => {
+import { FC, ReactNode, MouseEvent } from "react";
+
+// Иконки предоставлены библиотекой "lucide-react"
+
+// Интерфейс для пропсов button
+interface ButtonProps {
+    buttonLabel?: string                                        // Текст button
+    buttonIcon?: ReactNode                                      // Иконка button
+    iconPosition?: "left" | "right" | "center"                  // Расположение иконки
+    buttonType?: "default" | "outline" | "link"                 // Тип button
+    buttonFunction?: (e: MouseEvent<HTMLButtonElement>) => void // Функция для выполнения
+    isDisabled?: boolean                                        // Заблокирована ли?
+    name?: string                                               // Имя button
+}
+
+export const Button: FC<ButtonProps> = ({ buttonLabel, buttonIcon, iconPosition = "left", buttonType = "default", buttonFunction, isDisabled = false, name = "custom-button" }) => {
+    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+        if (!isDisabled && buttonFunction)
+            buttonFunction(e)
+    }
+
     return (
-        <div>
+        <div className="custom-button-wrapper">
+            <button id={name}
+                    disabled={isDisabled}
+                    onClick={handleClick}
+                    className={`custom-button ${buttonType} ${isDisabled ? "disabled" : ""} ${iconPosition}`}>
+                    
+                {buttonIcon && (iconPosition === "left" || iconPosition === "center") && (<div className="custom-button-icon">{buttonIcon}</div>)}
 
+                {buttonLabel && iconPosition !== "center" && (<span className="custom-button-text">{buttonLabel}</span>)}
+                
+                {buttonIcon && iconPosition === "right" && (<div className="custom-button-icon">{buttonIcon}</div>)}
+            </button>
         </div>
     )
 }
