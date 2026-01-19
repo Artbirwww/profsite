072eccff1f98cb5a6622b0a66809768bc2f35d94
A1tBot <timofeyershovv@gmail.com>
2026-01-19 19:16:12 +0300

    Поправил внешний вид кода для страницы с персональной информацией

--- src/components/profile/PersonalInformation.tsx
+++ src/components/profile/PersonalInformation.tsx
@@ -1,26 +1,41 @@
-import { ChangeEvent, FC, useState } from "react";
-import { FieldInput } from "../ui/reusable/fieldInput";
-import { UserPen, MailOpen, School, Hash, CaseUpper, PersonStanding, CheckCheck } from "lucide-react";
-import { Button } from "../ui/reusable/button";
-import { Radio } from "../ui/reusable/radio";
-import { Dropdown } from "../ui/reusable/dropdown";
-import { DatePicker } from "../ui/reusable/datePicker";
-import { Temporal } from "@js-temporal/polyfill";
+import { ChangeEvent, FC, useCallback, useMemo, useState } from "react"
+import { FieldInput } from "../ui/reusable/fieldInput"
+import { UserPen, MailOpen, School, Hash, CaseUpper, PersonStanding, CheckCheck } from "lucide-react"
+import { Button } from "../ui/reusable/button"
+import { Radio } from "../ui/reusable/radio"
+import { Dropdown } from "../ui/reusable/dropdown"
+import { DatePicker } from "../ui/reusable/datePicker"
+import { Temporal } from "@js-temporal/polyfill"
+
+interface PersonalData {
+    gender?: string
+    surname?: string
+    name?: string
+    patronymic?: string
+    email?: string
+    nationality?: string | number
+    birthDate?: Temporal.PlainDate
+    school?: string
+    classNumber?: string | number
+    classLetter?: string
+}
+
 
 export const PersonalInformation: FC = () => {
-    const [name, setName] = useState<string>("")
-    const [surname, setSurname] = useState<string>("")
-    const [patronymic, setPatronymic] = useState<string>("")
-    const [gender, setGender] = useState<string>("")
-    const [email, setEmail] = useState<string>("")
-    const [nationality, setNationality] = useState<string | number>("")
-    const [birthDate, setBirthDate] = useState<Temporal.PlainDate>()
-    const [school, setSchool] = useState<string>("")
-    const [classNumber, setClassNumber] = useState<string | number>("")
-    const [classLetter, setClassLetter] = useState<string>("")
+    const [formData, setFormData] = useState<PersonalData>({
+        gender: "",
+        surname: "",
+        name: "",
+        patronymic: "",
+        email: "",
+        nationality: "",
+        school: "",
+        classNumber: "",
+        classLetter: "",
+    })
 
 
-    const classNumberOptions = [
+    const classNumberOptions = useMemo(() => [
         { value: 5, label: "5" },
         { value: 6, label: "6" },
         { value: 7, label: "7" },
@@ -28,151 +43,99 @@ export const PersonalInformation: FC = () => {
         { value: 9, label: "9" },
         { value: 10, label: "10" },
         { value: 11, label: "11" },
-    ]
+    ], [])
 
-    const nationalityOptions = [
+    const nationalityOptions = useMemo(() => [
         { value: "aboba", label: "aboba" },
         { value: "aboba1", label: "aboba1" },
         { value: "aboba2", label: "aboba2" },
-        { value: "aboba3", label: "aboba3" },
-        { value: "aboba4", label: "aboba4" },
-        { value: "aboba5", label: "aboba5" },
-        { value: "aboba6", label: "aboba6" },
-        { value: "aboba6", label: "aboba6" },
-        { value: "aboba6", label: "aboba6" },
-        { value: "aboba6", label: "aboba6" },
-        { value: "aboba6", label: "aboba6" },
-    ]
+    ], [])
 
-    const handleSaveClick = () => {
-        console.log("Data saved")
-    }
-
-    const handleBackClick = () => {
-        console.log("Back")
-    }
+    const updateField = useCallback((field: keyof PersonalData) => (value: string | number) => {
+        setFormData(prev => ({ ...prev, [field]: value }))
+    }, [])
 
     const handleRadioChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setGender(e.target.value)
+        setFormData(prev => ({ ...prev, gender: e.target.value }))
     }
 
-    const handleDateSelect = (date: Temporal.PlainDate) => {
-        setBirthDate(date)
+    const handleDateSelect = useCallback((date: Temporal.PlainDate) => {
+        setFormData(prev => ({ ...prev, birthDate: date }))
+    }, [])
+
+    const handleSaveClick = () => {
+        console.log("Saving data: ", formData)
     }
 
     return (
         <div className="profile-personal-container">
             <h3 className="profile-personal-title">Персональная информация</h3>
-
-            <div className="profile-separator"></div>
+            <div className="profile-separator" />
 
             <div className="profile-personal-inputs-cols">
+                {/* Секция Пола */}
                 <div className="profile-personal-inputs-row">
                     <fieldset>
                         <legend>Укажите Ваш пол *</legend>
 
-                        <Radio radioLabel={"Мужской"}
-                               radioGroup={"profile-user-gender"}
-                               radioValue={"male"}
-                               radioChecked={gender === "male"}
-                               radioOnChange={handleRadioChange}/>
+                        <div className="radio-group">
+                            <Radio radioLabel="Мужской" radioGroup="gender" radioValue="male"
+                                   radioChecked={formData.gender === "male"} radioOnChange={handleRadioChange} />
 
-                        <Radio radioLabel={"Женский"}
-                               radioGroup={"profile-user-gender"}
-                               radioValue={"female"}
-                               radioChecked={gender === "female"}
-                               radioOnChange={handleRadioChange}/>
+                            <Radio radioLabel="Женский" radioGroup="gender" radioValue="female"
+                                   radioChecked={formData.gender === "female"} radioOnChange={handleRadioChange} />
+                        </div>
                     </fieldset>
                 </div>
 
+                {/* ФИО */}
                 <div className="profile-personal-inputs-row">
-                    <div>
-                        <FieldInput inputLabel={"Имя"}
-                                    inputIcon={<UserPen size={20}/>}
-                                    inputPlaceholder={"Иван"}
-                                    isImportant={true}
-                                    inputValue={name}
-                                    inputOnChange={setName}/>
-                    </div>
+                    <FieldInput inputLabel="Фамилия" inputIcon={<UserPen size={20} />} inputPlaceholder="Иванов"
+                                isImportant inputValue={formData.surname} inputOnChange={updateField("surname")} />
 
-                    <div>
-                        <FieldInput inputLabel={"Фамилия"}
-                                    inputPlaceholder={"Иванов"}
-                                    isImportant={true}
-                                    inputValue={surname}
-                                    inputOnChange={setSurname}/>
-                    </div>
+                    <FieldInput inputLabel="Имя" inputPlaceholder="Иван"
+                                isImportant inputValue={formData.name} inputOnChange={updateField("name")} />
 
-                    <div>
-                        <FieldInput inputLabel={"Отчество"}
-                                    inputPlaceholder={"Иванович"}
-                                    isImportant={true}
-                                    inputValue={patronymic}
-                                    inputOnChange={setPatronymic}/>
-                    </div>
+                    <FieldInput inputLabel="Отчество" inputPlaceholder="Иванович"
+                                isImportant inputValue={formData.patronymic} inputOnChange={updateField("patronymic")} />
                 </div>
 
+                {/* Контакты */}
                 <div className="profile-personal-inputs-row">
-                    <div>
-                        <FieldInput inputLabel={"Электронная почта"}
-                                    inputIcon={<MailOpen size={20}/>}
-                                    inputPlaceholder={"example@gmail.com"}
-                                    isImportant={true}
-                                    inputValue={email}
-                                    inputOnChange={setEmail}/>
-                    </div>
+                    <FieldInput inputLabel="Электронная почта" inputIcon={<MailOpen size={20} />}
+                                inputPlaceholder="example@gmail.com" isImportant
+                                inputValue={formData.email} inputOnChange={updateField("email")} />
                 </div>
 
+                {/* Национальность и Дата */}
                 <div className="profile-personal-inputs-row">
-                    <div>
-                        <Dropdown dropdownLabel={"Национальность"}
-                                  dropdownOptions={nationalityOptions}
-                                  dropdownSelected={nationality}
-                                  dropdownIcon={<PersonStanding size={20}/>}
-                                  optionOnSelect={(opt) => setNationality(opt.value)}/>
-                    </div>
+                    <Dropdown dropdownLabel="Национальность" dropdownIcon={<PersonStanding size={20} />}
+                              dropdownOptions={nationalityOptions} dropdownSelected={formData.nationality}
+                              optionOnSelect={(opt) => updateField("nationality")(opt.value)} />
 
-                    <div>
-                        <DatePicker datePickerLabel={"Дата рождения"}
-                                    datePickerSelected={birthDate}
-                                    onDateSelect={handleDateSelect}/>
-                    </div>
+                    <DatePicker datePickerLabel="Дата рождения" datePickerSelected={formData.birthDate}
+                                onDateSelect={handleDateSelect} />
                 </div>
 
+                {/* Школа и Класс */}
                 <div className="profile-personal-inputs-row">
-                    <div>
-                        <FieldInput inputLabel={"Школа"}
-                                    inputIcon={<School size={20}/>}
-                                    inputPlaceholder={"МБОУ города Абакана СОШ №9"}
-                                    inputValue={school}
-                                    inputOnChange={setSchool}/>
-                    </div>
+                    <FieldInput inputLabel="Школа" inputIcon={<School size={20} />}
+                                inputPlaceholder="МБОУ СОШ №9" inputValue={formData.school}
+                                inputOnChange={updateField("school")} />
 
-                    <div>
-                        <Dropdown dropdownLabel={"Номер класса"}
-                                  dropdownOptions={classNumberOptions}
-                                  dropdownSelected={classNumber}
-                                  dropdownIcon={<Hash size={20}/>}
-                                  optionOnSelect={(opt) => setClassNumber(opt.value)}/>
-                    </div>
+                    <Dropdown dropdownLabel="Номер класса" dropdownIcon={<Hash size={20} />}
+                              dropdownOptions={classNumberOptions} dropdownSelected={formData.classNumber}
+                              optionOnSelect={(opt) => updateField("classNumber")(opt.value)} />
 
-                    <div>
-                        <FieldInput inputLabel={"Буква класса"}
-                                    inputIcon={<CaseUpper size={20}/>}
-                                    inputPlaceholder={"а-я"}
-                                    inputValue={classLetter}
-                                    inputOnChange={setClassLetter}/>
-                    </div>
+                    <FieldInput inputLabel="Буква класса" inputIcon={<CaseUpper size={20} />}
+                                inputPlaceholder="а-я" inputValue={formData.classLetter}
+                                inputOnChange={updateField("classLetter")} />
                 </div>
 
-                <div className="profile-personal-inputs-row">
-
-                    <div>
-                        <Button buttonLabel={"Сохранить"}
-                                buttonIcon={<CheckCheck size={20}/>}
-                                isDisabled={false}
-                                buttonFunction={handleSaveClick}/>
-                    </div>
+                {/* Кнопка сохранения */}
+                <div className="profile-personal-inputs-row button-input-row">
+                    <Button buttonLabel="Сохранить" buttonIcon={<CheckCheck size={20} />}
+                            buttonFunction={handleSaveClick} />
                 </div>
             </div>
         </div>
