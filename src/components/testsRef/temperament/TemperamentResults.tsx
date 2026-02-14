f8de650fe4c389e52d6d144e9b46d28fae0c4d26
lubuntum <lubuntum@gmail.com>
2026-02-14 10:43:09 +0700

    Компонент для просмотра результатов и отправки данных на сервер

---
+++ src/components/testsRef/temperament/TemperamentResults.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import { PsychTestRequest, PsychTestResponse } from "../../../testApi"
+import { calculateParams } from "./temperamentResultCalc"
+import { TemperamentFormA, TemperamentFormB, TemperamentOption } from "./temperamentData"
+import { useAuth } from "../../../contexts/AuthContext"
+import { testApi } from "../../../services/api/testApi"
+import toast, { Toaster } from "react-hot-toast"
+
+/**
+ * TODO
+ * 1. Запросить с сервака данные о темперамента пользователя
+ * 2. Вызвать функцию подсчета результатов с данных сервера
+ * 3. Отобразить то, что подсчиталось пользователю
+ * 
+ */
+export const TemperamentResults = () => {
+    const location = useLocation()
+    const {getToken} = useAuth()
+    const [psychTest, setPsychTest] = useState<PsychTestRequest | null>(null)
+    const navigationOptions: TemperamentOption[] = location.state?.options || []
+    const temperamentFormData: TemperamentOption[] = location.state?.temperamentForm === 'A' ? TemperamentFormA : TemperamentFormB
+    useEffect(() => {
+        if (navigationOptions.length > 0) {
+            setPsychTest(calculateParams(navigationOptions, 0, temperamentFormData))
+            return
+        }
+    }, [])
+    useEffect(() => {
+        const createTest = async () => {
+            try {
+                const token = getToken()
+                if (!token || !psychTest) return
+                const responseData = await testApi.createTest(token, psychTest)
+            } catch(err) {
+                console.error(err)
+                toast.error("Не удалось связаться с сервером, повторите попытку позже")
+            }
+        }
+        createTest()
+        
+    }, [psychTest])
+    if (!psychTest)
+        return <p>Загрузка</p>
+    return(<>
+        <p>Результаты теста темперамента</p>
+        {psychTest.psychParams.map(param => (
+            <p>{param.name} : {param.param}</p>
+        ))}
+        <Toaster />
+    </>)
+
+}
