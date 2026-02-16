40c94721d96afa2dfa88e6d5979d8eb1128d65e3
lubuntum <lubuntum@gmail.com>
2026-02-16 15:02:07 +0700

    Исправлен баг в подсчете результатов темперамента

--- src/components/testsPage/temperament/temperamentResultCalc.ts
+++ src/components/testsPage/temperament/temperamentResultCalc.ts
@@ -8,17 +8,25 @@ export const calculateParams = (options: TemperamentOption[], completeTime: numb
         psychParams: [{ name: "extrav_introver_score", param: 0 }, { name: "neirotizm_score", param: 0 }, { name: "sincerity_score", param: 0 }]
     }
 
-    options.forEach(option => {
-        if (option.answer) {
-            if (option.param === 'extrav_introver_score')
-                results.psychParams[0].param += 1
-            else if (option.param === 'neirotizm_score')
-                results.psychParams[1].param += 1
-            else if (option.param === 'sincerity_score')
-                results.psychParams[2].param += 1
+    options.forEach(userAnswer => {
+        // Находим эталонный вопрос с таким же ID
+        const presetQuestion = temperamentFormData.find(q => q.id === userAnswer.id);
+        
+        if (presetQuestion) {
+            // Проверяем: совпадает ли ответ пользователя с тем ответом, который дает балл?
+            if (userAnswer.answer === presetQuestion.answer) {
+                if (presetQuestion.param === 'extrav_introver_score') {
+                    results.psychParams[0].param += 1;
+                } else if (presetQuestion.param === 'neirotizm_score') {
+                    results.psychParams[1].param += 1;
+                } else if (presetQuestion.param === 'sincerity_score') {
+                    results.psychParams[2].param += 1;
+                }
+            }
         }
-    })
-    return results
+    });
+
+    return results;
 }
 
 export const calculateTemperament = (data: TestResultResponse) => {
