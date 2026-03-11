2d03a36d4fa315395d47b763b57e8ebcfa87435c
lubuntum <lubuntum@gmail.com>
2026-03-11 17:00:30 +0700

    Адаптивный прогресс бар для тестов (в шаблонах)

---
+++ src/components/testsPage/generalTemplates/progressBar/ProgressBar.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+import './progress-bar.css'
+interface ProgressBarProps {
+    currentTaskNumber: number
+    total: number, 
+}
+
+export const ProgressBar = ({currentTaskNumber, total} : ProgressBarProps) => {
+    const progress = Math.round((currentTaskNumber / total) * 100)
+    const progressColor = (progress: number) => {
+        if (progress >= 0 && progress < 30) return 'red'
+        if (progress >= 30 && progress < 70) return 'orange'
+        if (progress >= 70 && progress < 85) return 'yellowgreen'
+        if (progress >= 85) return 'green'
+    }
+    return (
+    <div className="progress-bar-wrapper">
+        <div className="progress-bar" 
+            style={{width: `${progress}%`, 
+                    backgroundColor: progressColor(progress)}}>
+
+        </div>
+    </div>)
+}
