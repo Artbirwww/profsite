105f06b7e350aa59012c55623dbaba1490618cbc
lubuntum <lubuntum@gmail.com>
2026-03-18 14:22:06 +0700

    Предупреждение заполнить ЛК при первом входе

--- src/components/testsPage/TestViewer.tsx
+++ src/components/testsPage/TestViewer.tsx
@@ -1,17 +1,30 @@
+import toast, { Toaster } from "react-hot-toast"
+import { useAuth } from "../../contexts/AuthContext"
 import "./css/testsViewerStyles.css"
 
 import { Outlet } from "react-router-dom"
+import { useEffect } from "react"
 
 export const TestViewer = () => {
+    const {isFirstLogin} = useAuth()
+    useEffect(() => {
+        if (isFirstLogin()) toast("Перед выполнением тестов обязательно заполните профиль", {
+            style: {backgroundColor: "orange"},
+            duration: 4500
+        })
+    }, [])
     return (
-        <div
-            className="test-viewer-wrapper">
-
+        <>
             <div
-                className="test-outlet">
+                className="test-viewer-wrapper">
 
-                <Outlet />
+                <div
+                    className="test-outlet">
+
+                    <Outlet />
+                </div>
             </div>
-        </div>
+           
+        </>
     )
 }
