b216defc5ea569577d2e847052d99714b28c4ec5
lubuntum <lubuntum@gmail.com>
2026-03-27 17:09:49 +0700

    Убрал лог

--- src/services/api/authApi.ts
+++ src/services/api/authApi.ts
@@ -41,7 +41,6 @@ export const authApi = {
   login: async (email: string, password: string) => {
     try {
       const response = await api.post('/api/auth/login', {"email": email, "password": password})
-      console.log(response.data)
       return response.data
     } catch(err) {
       console.error(err)
