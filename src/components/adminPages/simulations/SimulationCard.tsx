84128a922f96d9ad231b97eb0a98841e5dd324cb
lubuntum <lubuntum@gmail.com>
2026-03-27 17:09:26 +0700

    Отобразил имена файлов в карточке сим

--- src/components/adminPages/simulations/SimulationCard.tsx
+++ src/components/adminPages/simulations/SimulationCard.tsx
@@ -1,3 +1,4 @@
+import path from "path"
 import api from "../../../services/api/api"
 import { BASE_URL } from "../../../services/api/baseUrl"
 import { SimulationResponse } from "../../../types/simulation/Simulation"
@@ -7,13 +8,13 @@ interface SimulationCardProps {
     simulation: SimulationResponse
 }
 export const SimulationCard = ({simulation}: SimulationCardProps) => {
-
     return( 
         <div key={simulation.id} className="simulation-card">
             <h3>Логин: {simulation.simulation.email}</h3>
-            <p>Профессия {simulation.simulation.profession} в {simulation.simulation.simulationType}</p>
-            <p><a className="download-link" href={`${BASE_URL}/${simulation.simulation.filePath}`}>Скачать файл симуляции </a></p>
-            <div className="simulation-footer">
+            <p>Профессия: {simulation.simulation.profession ? simulation.simulation.profession : "-"}</p>
+            <p>Тип симуляции: {simulation.simulation.simulationType ? simulation.simulation.simulationType : "-"}</p>
+            <p><a className="download-link" download={true} href={`${BASE_URL}/${simulation.simulation.filePath}`}>Скачать файл симуляции  {simulation.simulation.filePath.split("/").pop()}</a></p>
+            <div className="simulations-footer">
                 <small>Дата начала: {simulation.simulation.startSimulation}</small>
                 <small>Дата завершения: {simulation.simulation.endSimulation}</small>
             </div>
