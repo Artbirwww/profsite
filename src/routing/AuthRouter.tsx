5a8bb45ca04fbefb2af4b32ef8e8799dd3c2ad1f
lubuntum <lubuntum@gmail.com>
2026-04-13 03:07:00 +0700

    Немного упростил проверку, но все равно периодически отлавливаю баг (необходимо тестить или править состояние токена)

--- src/routing/AuthRouter.tsx
+++ src/routing/AuthRouter.tsx
@@ -4,19 +4,12 @@ import React from 'react';
 export const AuthRouter: React.FC = () => {
     const {token, getToken, isLoading} = useAuth()
     const navigate = useNavigate()
-    const checkForToken = () => {
-        //even if error occured go to top page (getToken use in many scenarious)
-        try {
-            if (getToken()) return true
-        } catch(err) {
-            return true
-        }
-    }
+    
     if (isLoading) return(
         <>
             <p>Загрузка...</p>
         </>)
-    if (checkForToken()) {
+    if (getToken()) {
         return <Navigate to={'/'} />
     }
 
