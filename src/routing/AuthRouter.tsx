87a59d24dfde7f634972ccc48423c67fd075291f
lubuntum <lubuntum@gmail.com>
2026-04-15 17:57:11 +0700

    Проверка ключа на срок годности (если он истек, но есть в куках)

--- src/routing/AuthRouter.tsx
+++ src/routing/AuthRouter.tsx
@@ -2,14 +2,14 @@ import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom"
 import { useAuth } from "../contexts/AuthContext"
 import React from 'react';
 export const AuthRouter: React.FC = () => {
-    const {token, getToken, isLoading} = useAuth()
+    const {token, getToken, isLoading, isTokenExpired} = useAuth()
     const navigate = useNavigate()
     
     if (isLoading) return(
         <>
             <p>Загрузка...</p>
         </>)
-    if (getToken()) {
+    if (getToken() && !isTokenExpired()) {
         return <Navigate to={'/'} />
     }
 
