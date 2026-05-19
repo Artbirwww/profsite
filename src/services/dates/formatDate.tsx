94ea16437c3bcb9ebea0e9fab8ae0cf14475a0e4
lubuntum <lubuntum@gmail.com>
2026-05-19 13:09:20 +0700

    утилити для преобразования даты

--- src/services/dates/formatDate.tsx
+++ src/services/dates/formatDate.tsx
@@ -1,4 +1,5 @@
-
+import { Temporal } from "@js-temporal/polyfill"
+//usage formatDateRU("01-25-2025") -> '25.01.2025 00:00:00'
 export const formatDateRU = (dateStr: string | undefined) => {
     if (!dateStr) return "Дата не найдена"
     const date = new Date(dateStr)
@@ -11,4 +12,10 @@ export const formatDateRU = (dateStr: string | undefined) => {
     const seconds = date.getSeconds().toString().padStart(2, '0')
 
     return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`
+}
+//usage "01-25-2026", false -> "01-25-2026 00:00:00"
+export const formatDateToDateTime = (dateStr: string, isEndOfDay: boolean = false) => {
+    const date = Temporal.PlainDate.from(dateStr)
+    const time = isEndOfDay ? "23:59:59" : "00:00:00"
+    return `${date.toString()}T${time}`
 }
