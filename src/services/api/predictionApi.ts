fef9c68c12fa5ef0ba59e6223f4f0dd4b0cc4385
lubuntum <lubuntum@gmail.com>
2026-06-01 15:55:08 +0700

    api для загрузки файла результатов

--- src/services/api/predictionApi.ts
+++ src/services/api/predictionApi.ts
@@ -1,4 +1,9 @@
+import axios from "axios"
+import api from "./api"
 
 export const predictionAPI = {
-    
+    getPredictionJson : async (filePath: string) => {
+        const response = await api.get(filePath)
+        return response.data
+    }
 }
