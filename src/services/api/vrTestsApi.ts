db65ad8c69776dffdb389aec208d0c484b815ffa
lubuntum <lubuntum@gmail.com>
2026-06-22 05:38:03 +0700

    апи для работы с вр тестами

---
+++ src/services/api/vrTestsApi.ts
@@ -0,0 +1,30 @@
+import { VRTest } from "../../types/vrTests/VRTest"
+import api from "./api"
+
+export const vrTestApi = {
+    createTest: async (token: string, testData: VRTest) => {
+        const response = await api.post(`api/vr-tests`, testData,  {
+            headers: {
+                Authorization: token
+            }
+        })
+        return response.data
+    }, 
+    getMyTests: async (token: string) => {
+        const response = await api.get(`api/vr-tests/my-tests`, {
+            headers: {Authorization: token}
+        })
+        return response.data
+    },
+    getMyTestsByProfessionId: async (token: string, professionId: string) => {
+        const response = await api.get(`api/vr-tests/my-tests/profession/${professionId}`, {
+            headers: {Authorization: token}
+        })
+        return response.data
+    },
+    resetTests: async (token: string, professionId: string) => {
+        const response = await api.delete(`api/vr-tests/my-tests/profession/${professionId}`, {
+            headers: {Authorization: token}
+        })
+    }
+}
