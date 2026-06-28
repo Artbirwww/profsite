a44a04ade01815827fd6e317f8528a94acaf072e
lubuntum <lubuntum@gmail.com>
2026-06-29 02:30:09 +0700

    Типы для работы с компаниями

---
+++ src/types/company/Company.ts
@@ -0,0 +1,10 @@
+
+export interface Company {
+    id: number
+    name: string
+    inn: string
+    ogrn: string
+    address: string
+    phone: string
+    email: string
+}
---
+++ src/types/company/HRManager.ts
@@ -0,0 +1,34 @@
+
+//entitiy returned for HR
+export interface HRManager {
+    accountId: number
+    companyId: number
+    email: string
+    fullName: string
+    companyName: string
+}
+//used when admin creates hr
+export interface HRManagerRequest {
+    email: string
+    password: string
+    role: string
+    name: string
+    surname: string
+    patronymic: string
+    companyName: string
+    companyInn: string
+    companyOgrn: string
+    companyAddress: string
+    companyPhone: string
+    companyEmail: string
+}
+//used when hr creates specialist for company
+export interface CreateSpecialistRequest {
+    email: string
+    password: string
+    role?: string
+    name: string
+    surname: string
+    patronymic: string,
+    companyName?: string
+}
